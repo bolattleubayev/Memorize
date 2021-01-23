c9fc8d840c1aae93b7ea43bed730383e4ac78bc9
Bolat Tleubayev <bolat.tleubayev@nu.edu.kz>
2021-01-23 03:56:08 +0100

    Lecture 1 end

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,12 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return RoundedRectangle(cornerRadius: 10.0)
-//        return Text("👻")
+        return HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
 
-
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
 
 
 
