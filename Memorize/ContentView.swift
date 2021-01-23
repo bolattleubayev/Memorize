27bbc589695706d9eeeda7ab7cb30b67e8dbebae
Bolat Tleubayev <bolat.tleubayev@nu.edu.kz>
2021-01-23 02:35:13 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by macbook on 23/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        return RoundedRectangle(cornerRadius: 10.0)
+//        return Text("👻")
+    }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
